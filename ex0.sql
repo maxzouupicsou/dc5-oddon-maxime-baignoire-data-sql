fe64d1ee6bb7a13fe6ec07dab9bf88fe3eebc2c2
maxouupicsou <144345958+maxzouupicsou@users.noreply.github.com>
2023-11-07 14:49:59 +0100

    MAJ indentation

--- ex0.sql
+++ ex0.sql
@@ -1,2 +1,5 @@
 /*Trouvez tous les pays et les codes connus les afficher en tant que “pays” et “libellé” */
-SELECT code AS Libellé , country AS pays FROM country;
+SELECT code
+AS Libellé , country
+AS pays
+FROM country;
