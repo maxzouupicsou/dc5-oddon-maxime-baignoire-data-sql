6c86e152707d3309ea036222acc63f206dc6e555
maxouupicsou <144345958+maxzouupicsou@users.noreply.github.com>
2023-11-07 16:26:03 +0100

    Interrogation de la BDD afin tous les officiers et intermédiaires qui ont une relation

---
+++ ex8.sql
@@ -0,0 +1,5 @@
+/* Trouver tous les officiers et intermédiaires qui ont une association */
+SELECT intermediary.name AS inter_name, officer.name AS offi_name
+FROM assoc_inter_offi
+JOIN intermediary ON intermediary.id = assoc_inter_offi.inter
+JOIN officer ON officer.id = assoc_inter_offi.officer;
